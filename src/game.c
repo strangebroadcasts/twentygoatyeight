39bc40086f45ee12de5192d240d224b6b9e2667b
Jørgen Lund <strangebroadcasts@users.noreply.github.com>
2018-04-02 22:24:04 +0200

    Implement merging

--- src/game.c
+++ src/game.c
@@ -20,7 +20,6 @@ void shift(unsigned char board[4][4], unsigned char direction)
                     }
                 }
             }
-
             break;
             case MOVE_RIGHT:
             for(x = 0; x < 4; x++)
@@ -68,14 +67,60 @@ void shift(unsigned char board[4][4], unsigned char direction)
 
 void merge(unsigned char board[4][4], unsigned char direction)
 {
+    unsigned char x, y; 
     switch (direction) {
         case MOVE_LEFT:
+        for(x = 0; x < 4; x++)
+        {
+            for(y = 1; y < 4; y++)
+            {
+                if(board[x][y-1] == board[x][y] && board[x][y] != 0)
+                {
+                    board[x][y-1] = board[x][y] + 1;
+                    board[x][y] = 0;
+                }
+            }
+        }
         break;
         case MOVE_RIGHT:
+        for(x = 0; x < 4; x++)
+        {
+            for(y = 3; y > 0; y--)
+            {
+                if(board[x][y] == board[x][y-1] && board[x][y] != 0)
+                {
+                    board[x][y] = board[x][y-1] + 1;
+                    board[x][y-1] = 0;
+                }
+            }
+        }
         break;
         case MOVE_UP:
+        for(y = 0; y < 4; y++)
+        {
+            for(x = 1; x < 4; x++)
+            {
+                if(board[x-1][y] == board[x][y] && board[x][y] != 0)
+                {
+                    board[x-1][y] = board[x][y] + 1;
+                    board[x][y] = 0;
+                }
+            }
+        }
+        
         break;
         case MOVE_DOWN:
+        for(y = 0; y < 4; y++)
+        {
+            for(x = 0; x < 3; x++)
+            {
+                if(board[x+1][y] == board[x][y] && board[x][y] != 0)
+                {
+                    board[x+1][y] = board[x][y] + 1;
+                    board[x][y] = 0;
+                }
+            }
+        }
         break;
     }
 }
